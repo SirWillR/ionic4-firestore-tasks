ac7a9c94eb52fbb03809fb3bb7247701f5015ac5
SirWill <dev_will@hotmail.com>
2019-10-13 15:10:00 -0300

    AuthService: armazenando o AuthState do Firebase

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
 import { User, AuthProvider, AuthOptions } from './auth.types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private afAuth: AngularFireAuth) {}
+  authState$: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.authState$ = this.afAuth.authState;
+  }
 
   authenticate({ isSignIn, provider, user }: AuthOptions): Promise<auth.UserCredential> {
     let operation: Promise<auth.UserCredential>;
