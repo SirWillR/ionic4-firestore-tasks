7dd48c71c9d00b9d8669623bc74c0937e0848d2e
SirWill <dev_will@hotmail.com>
2019-10-13 13:42:33 -0300

    Integrando Ionic e Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCPyRaQEimhf6Nx7bXoBbM6JievryUcmkk',
+    authDomain: 'ionic-firestore-tasks-3529a.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-tasks-3529a.firebaseio.com',
+    projectId: 'ionic-firestore-tasks-3529a',
+    storageBucket: 'ionic-firestore-tasks-3529a.appspot.com',
+    messagingSenderId: '730794176500',
+    appId: '1:730794176500:web:df144c63041a6109be93a1',
+    measurementId: 'G-R8RZ2SD340'
+  }
 };
 
 /*
