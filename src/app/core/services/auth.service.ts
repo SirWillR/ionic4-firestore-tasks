e3025e9748057f6f51d503ad15f40dd8fc84366f
SirWill <dev_will@hotmail.com>
2019-10-13 15:13:44 -0300

    AuthService: Observable para determinar se usuário está autenticado

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User, AuthProvider, AuthOptions } from './auth.types';
 
 @Injectable({
@@ -12,6 +13,10 @@ export class AuthService {
 
   constructor(private afAuth: AngularFireAuth) {
     this.authState$ = this.afAuth.authState;
+  }
+
+  get isAuthenticated(): Observable<boolean> {
+    return this.authState$.pipe(map(user => user !== null));
   }
 
   authenticate({ isSignIn, provider, user }: AuthOptions): Promise<auth.UserCredential> {
