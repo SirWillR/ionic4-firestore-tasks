f95c3054e1bfbd743b2ac8ceb56ae6757738c03f
SirWill <dev_will@hotmail.com>
2019-10-13 22:18:07 -0300

    Adicionando loading na TaskList

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Task } from '../../models/task.model';
 import { TasksService } from '../../services/tasks.service';
 import { NavController } from '@ionic/angular';
+import { OverlayService } from 'src/app/core/services/overlay.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -12,10 +13,20 @@ import { NavController } from '@ionic/angular';
 export class TasksListPage {
   tasks$: Observable<Task[]>;
 
-  constructor(private navCtrl: NavController, private tasksService: TasksService) {}
+  constructor(
+    private navCtrl: NavController,
+    private tasksService: TasksService,
+    private overlayService: OverlayService
+  ) {}
 
   ionViewDidEnter(): void {
+    this.loadTasks();
+  }
+
+  private async loadTasks() {
+    const loading = await this.overlayService.loading();
     this.tasks$ = this.tasksService.getAll();
+    loading.dismiss();
   }
 
   onUpdate(task: Task): void {
