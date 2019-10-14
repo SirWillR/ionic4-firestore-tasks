34894e8db930a37da6e6232c0c7679a1f0071256
SirWill <dev_will@hotmail.com>
2019-10-14 08:53:13 -0300

    Criando Component reusável para IonMenuToggle

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
 
 @NgModule({
-  exports: [CommonModule, ReactiveFormsModule, IonicModule]
+  declarations: [MenuToggleComponent],
+  imports: [IonicModule],
+  exports: [CommonModule, ReactiveFormsModule, IonicModule, MenuToggleComponent]
 })
 export class SharedModule {}
