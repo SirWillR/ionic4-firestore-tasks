1e9f1bf7cf1b7d881fa47bab69ac1257e4702c1a
SirWill <dev_will@hotmail.com>
2019-10-15 09:31:56 -0300

    Configurações do Firebase para Produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCPyRaQEimhf6Nx7bXoBbM6JievryUcmkk',
+    authDomain: 'ionic-firestore-tasks-3529a.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-tasks-3529a.firebaseio.com',
+    projectId: 'ionic-firestore-tasks-3529a',
+    storageBucket: 'ionic-firestore-tasks-3529a.appspot.com',
+    messagingSenderId: '730794176500',
+    appId: '1:730794176500:web:df144c63041a6109be93a1',
+    measurementId: 'G-R8RZ2SD340'
+  }
 };
